41a4fdf14e961052b45c6d3d64e3be77bceea37d
Svetlik-B <svetlik.igorevna@gmail.com>
2025-04-02 15:09:11 +0400

    перенести кнопки в navigationBar

--- Tracker/TrackerViewController.swift
+++ Tracker/TrackerViewController.swift
@@ -1,55 +1,36 @@
 import UIKit
 
 class TrackerViewController: UIViewController {
-    let dateLabel = UILabel()
     let dateFormatter = DateFormatter()
     override func viewDidLoad() {
         super.viewDidLoad()
         setupDateLabel()
         updateDate()
-
-        let addButton = UIButton(type: .system)
-        let label = UILabel()
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.tintColor = .black
+        navigationItem.title = "Трекеры"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: UIImage(named: "plus"),
+            style: .plain,
+            target: nil,
+            action: nil
+        )
+        
         let logoImageView = UIImageView(image: UIImage(named: "1"))
         let questionLabel = UILabel()
         let searchBar = UISearchBar()
 
-        [addButton, label, logoImageView, questionLabel, searchBar].forEach {
+        [logoImageView, questionLabel, searchBar].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             view.addSubview($0)
         }
-
-        addButton.setImage(UIImage(named: "plus"), for: .normal)
-        addButton.titleLabel?.font = UIFont.systemFont(ofSize: 36, weight: .bold)
-        addButton.setTitleColor(.white, for: .normal)
-        addButton.tintColor = .black
-        addButton.clipsToBounds = true
-
-        NSLayoutConstraint.activate([
-            addButton.leadingAnchor.constraint(
-                equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-
-            addButton.topAnchor.constraint(
-                equalTo: view.safeAreaLayoutGuide.topAnchor),
-
-            addButton.widthAnchor.constraint(equalToConstant: 42),
-            addButton.heightAnchor.constraint(equalToConstant: 42),
-        ])
-
-        label.text = "Трекеры"
-        label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 34, weight: .semibold)
-
-        NSLayoutConstraint.activate([
-            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            label.topAnchor.constraint(equalTo: addButton.bottomAnchor, constant: 2),
-        ])
 
         searchBar.placeholder = "Поиск"
         searchBar.searchBarStyle = .minimal
 
         NSLayoutConstraint.activate([
-            searchBar.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 7),
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
         ])
@@ -80,29 +61,16 @@ class TrackerViewController: UIViewController {
         dateFormatter.dateFormat = "dd.MM.yy"
         dateFormatter.locale = Locale(identifier: "ru_RU")
 
-        dateLabel.font = UIFont.systemFont(ofSize: 17, weight: .light)
-        dateLabel.textAlignment = .center
-        dateLabel.textColor = .black
-        dateLabel.backgroundColor = .quaternarySystemFill
-        dateLabel.layer.cornerRadius = 7
-        dateLabel.layer.masksToBounds = true
-
-        dateLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(dateLabel)
-
-        NSLayoutConstraint.activate([
-            dateLabel.trailingAnchor.constraint(
-                equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
-
-            dateLabel.topAnchor.constraint(
-                equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 1),
-
-            dateLabel.widthAnchor.constraint(equalToConstant: 77),
-            dateLabel.heightAnchor.constraint(equalToConstant: 34),
-        ])
+//        dateLabel.backgroundColor = .quaternarySystemFill
     }
     func updateDate() {
         let currentDate = Date()
-        dateLabel.text = dateFormatter.string(from: currentDate)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: dateFormatter.string(from: currentDate),
+            style: .plain,
+            target: nil,
+            action: nil
+        )
+
     }
 }
